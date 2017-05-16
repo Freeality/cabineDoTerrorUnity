ea2a96c6232f0bf016171fbdd2eb6856c2da4280
Pedro Brandao <pcrbrandao@gmail.com>
2017-05-16 18:08:40 -0300

    Sangue, ajustes e o ponto final

---
+++ Assets/Scripts/EndOnTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOnTrigger : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (IsPlayer (other)) {
+			Quit ();
+		}
+	}
+
+	bool IsPlayer(Collider other) {
+		
+		if (other.gameObject.tag == "Player") {
+			return true;
+		}
+
+		return false;
+	}
+
+	void Quit() {
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
